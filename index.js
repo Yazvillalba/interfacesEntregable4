6726ff3c1a750f43378d2a5096c871d294b0f108
Marlene Carli <maarlen.car@hotmail.com>
2023-06-26 21:04:26 -0300

    Added js

---
+++ index.js
@@ -0,0 +1,6 @@
+import { navbar } from "./navbar.js";
+
+const body = document.getElementsByTagName("body");
+
+body[0].innerHTML = navbar;
+console.log('body', body)
---
+++ navbar.js
@@ -0,0 +1,86 @@
+export const navbar = `
+<img src="img/WhatsApp Image 2023-06-26 at 18.26.13.jpeg" alt="" class="img-banner">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Sobre nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="noticias.html">Noticias</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Adopciones</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="perdidos.html">Perdidos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Colaborar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><img src="img/acceso.png" alt="" class="img-access"></a>
+            </li>
+            <!-- <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Dropdown link
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li> -->
+        </ul>
+    </div>
+</div>
+</nav>
+    
+    <div>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="img/predio apoaa.jpg" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">PREDIO APOAA</h5>
+                  <p class="card-text">Contamos con un predio cedido en comodato por la Municipalidad de Olavarría, el cual posee varios caniles.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="img/galgo.webp" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">GALGOS RESCATADOS</h5>
+                  <p class="card-text">“En olavarria hemos rescatado a mas de cien perros galgos”. Una de las personas que más trabaja en este tema en nuestra protectora es Goga Álvarez, ella tiene un amor especial por los galgos y en este momento tiene en su casa cinco rescatados.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+    </div>
+`;
+
+
+export class Navbar {
+
+}
