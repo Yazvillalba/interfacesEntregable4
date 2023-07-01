f564c3e25f3a9bc4093e3a94d47d4c4cc44b653e
Marlene Carli <maarlen.car@hotmail.com>
2023-07-01 14:46:06 -0300

    modificaciones en navbar

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
-import { navbar } from "./navbar.js";
+import { construirNavbar } from "./navbar.js";
 
+const location = window.location;
 const body = document.getElementsByTagName("body");
 
+console.log("location", location);
+
+const direcciones = [
+    { navName: "Home", path: "index", active: false },
+    { navName: "Perdidos", path: "perdidos", active: false },
+    { navName: "Noticias", path: "noticias", active: false },
+    { navName: "Adopciones", path: "perritos", active: false },
+    {
+        navName: `<img src="img/acceso.png" alt=""
+    class="img-access">`,
+        path: "login",
+        active: false,
+    },
+];
+
+function getHTMLFileName(path = "") {
+    return path.replace("/", "").replace(".html", "");
+}
+
+const path = getHTMLFileName(location.pathname);
+
+const direcc = direcciones.map((direccion) => {
+    if (direccion.path === path) {
+        direccion.active = true;
+    }
+    return direccion;
+});
+console.log("direcc", direcc);
+
+const navElements = construirNavbar(direcc);
+
+console.log("navElements", navElements);
+
+const navListItemContainer = document.getElementById("nav-list");
+navListItemContainer.innerHTML = navElements;
+
+console.log("body", body);
+
+const htmlBody = body.innerHTML;
+
+console.log('htmlBody', htmlBody)
+
+console.log('banner', banner)
+
+body.innerHTML = banner + htmlBody
+
 // body[0].innerHTML = navbar;
-console.log('body', body);
+console.log("body", body);
+
--- navbar.js
+++ navbar.js
@@ -1,5 +1,70 @@
-export const navbar = `
 
-`;
+
+/*  
+ <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">APOAA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav" id="nav-list">
+                        <!-- <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Sobre nosotros</a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="noticias.html">Noticias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="perritos.html">Adopciones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="perdidos.html">Perdidos</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="#">Colaborar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacto</a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.html"><img src="img/acceso.png" alt=""
+                                    class="img-access"></a>
+                        </li>
+                        <!-- <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Dropdown link
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li> -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+*/
+
+const navItemContainer = document.getElementById('navbarNavDropdown');
+
+export function construirNavbar(elements) {
+    let nav = "";
+
+    elements.forEach(e => {
+        console.log(e)
+        nav += `<li class="nav-item ${e.active ? 'active' : '' }">
+                    <a class="nav-link" href="${e.path}.html">${e.navName}</a>
+                </li>`
+    });
+
+    return nav;
+}
 
 export class Navbar {}
